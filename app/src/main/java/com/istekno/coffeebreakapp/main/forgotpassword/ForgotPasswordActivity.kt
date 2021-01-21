31e173abf1fc3823cf814c7722a9364073d6bd17
ajizul22 <70998823+ajizul22@users.noreply.github.com>
2021-01-21 15:03:49 +0700

    validation

--- app/src/main/java/com/istekno/coffeebreakapp/main/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/main/forgotpassword/ForgotPasswordActivity.kt
@@ -3,9 +3,11 @@ package com.istekno.coffeebreakapp.main.forgotpassword
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.istekno.coffeebreakapp.R
 import com.istekno.coffeebreakapp.base.BaseActivity
 import com.istekno.coffeebreakapp.databinding.ActivityForgotPasswordBinding
+import com.istekno.coffeebreakapp.main.signup.SignupActivity
 
 class ForgotPasswordActivity : BaseActivity<ActivityForgotPasswordBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,31 @@ class ForgotPasswordActivity : BaseActivity<ActivityForgotPasswordBinding>() {
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
         )
+        viewListener()
+    }
+
+    private fun viewListener() {
+        binding.btnResendLink.setOnClickListener {
+            validation()
+        }
+    }
+
+    private fun validation() {
+        val inputEmail = binding.etEmail.text.toString()
+
+        if (inputEmail.isEmpty()) {
+            showToast(SignupActivity.FIELD_REQUIRED)
+            return
+        }
+
+        if (!inputEmail.contains('@') || !inputEmail.contains('.')) {
+            showToast(SignupActivity.FIELD_IS_NOT_VALID)
+            return
+        }
+        showToast("Success")
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
