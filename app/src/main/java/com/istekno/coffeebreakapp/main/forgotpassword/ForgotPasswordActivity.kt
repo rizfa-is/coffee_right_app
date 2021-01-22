ba6b48816bab9978bde75234a850f8731f0e6955
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-01-22 07:45:03 +0700

    fic bug

--- app/src/main/java/com/istekno/coffeebreakapp/main/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/main/forgotpassword/ForgotPasswordActivity.kt
@@ -1,11 +1,8 @@
 package com.istekno.coffeebreakapp.main.forgotpassword
 
 import android.os.Bundle
-<<<<<<< HEAD
 import android.view.WindowManager
 import android.widget.Toast
-=======
->>>>>>> feature-maincontentUI
 import com.istekno.coffeebreakapp.R
 import com.istekno.coffeebreakapp.base.BaseActivity
 import com.istekno.coffeebreakapp.databinding.ActivityForgotPasswordBinding
@@ -15,7 +12,6 @@ class ForgotPasswordActivity : BaseActivity<ActivityForgotPasswordBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setLayout = R.layout.activity_forgot_password
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
@@ -47,7 +43,5 @@ class ForgotPasswordActivity : BaseActivity<ActivityForgotPasswordBinding>() {
 
     private fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-=======
->>>>>>> feature-maincontentUI
     }
 }
