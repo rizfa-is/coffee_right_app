35af964fa1d4899985c22cc2af57e1e83f2bde8f
ajizul22 <70998823+ajizul22@users.noreply.github.com>
2021-01-25 21:43:16 +0700

    integrasi detail product

---
+++ app/src/main/java/com/istekno/coffeebreakapp/main/detailproduct/DetailProductResponse.kt
@@ -0,0 +1,18 @@
+package com.istekno.coffeebreakapp.main.detailproduct
+
+import com.google.gson.annotations.SerializedName
+
+data class DetailProductResponse(val success: Boolean, val message: String, val data: List<DataProduct>) {
+    data class DataProduct(
+            @SerializedName("pr_id") val productId: Int,
+            @SerializedName("dc_id") val discountId: Int,
+            @SerializedName("pr_name") val productName: String,
+            @SerializedName("pr_size") val productSize: String,
+            @SerializedName("pr_desc") val productDesc: String,
+            @SerializedName("pr_unit_price") val productPrice: String,
+            @SerializedName("pr_image") val productImage: String,
+            @SerializedName("pr_category") val productCategory: String,
+            @SerializedName("pr_created_at") val productCreated: String,
+            @SerializedName("pr_updated_at") val productUpdated: String
+    )
+}
