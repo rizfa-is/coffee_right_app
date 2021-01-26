9abfb76e542d9f0910272dbfdbc29c2e20d05249
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-01-27 00:27:45 +0700

    update login

--- app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/homepage/HomeResponse.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/homepage/HomeResponse.kt
@@ -7,10 +7,10 @@ data class HomeResponse(val success: Boolean, val message: String, val data: Lis
             @SerializedName("pr_id") val productId: Int,
             @SerializedName("dc_id") val discountId: Int,
             @SerializedName("pr_name") val productName: String,
-            @SerializedName("pr_size") val productSize: String,
             @SerializedName("pr_desc") val productDesc: String,
             @SerializedName("pr_unit_price") val productPrice: String,
             @SerializedName("pr_image") val productImage: String,
+            @SerializedName("pr_favorite") val productFavorite: String,
             @SerializedName("pr_category") val productCategory: String,
             @SerializedName("pr_created_at") val productCreated: String,
             @SerializedName("pr_updated_at") val productUpdated: String
