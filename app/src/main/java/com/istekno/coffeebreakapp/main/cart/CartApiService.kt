ad2d5cb3e4a87504a3ad91f19cdd2749849c9dc4
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2021-01-28 01:16:33 +0800

    add feature cart

---
+++ app/src/main/java/com/istekno/coffeebreakapp/main/cart/CartApiService.kt
@@ -0,0 +1,20 @@
+package com.istekno.coffeebreakapp.main.cart
+
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface CartApiService {
+
+    @GET("order/statusCart/{csId}")
+    suspend fun getListCartByCsId(@Path("csId") csId: Int) : CartResponse
+
+    @GET("order/statusCart/{csId}")
+    suspend fun getCartPriceByCsId(@Path("csId") csId: Int) : CartPriceResponse
+
+    @PUT("order/updateOrder/{orId}")
+    suspend fun updateOrderPlus(@Path("orId") orId : Int) : UpdateOrderResponse
+
+    @PUT("order/updateOrderMin/{orId}")
+    suspend fun updateOrderMin(@Path("orId") orId : Int) : UpdateOrderResponse
+}
