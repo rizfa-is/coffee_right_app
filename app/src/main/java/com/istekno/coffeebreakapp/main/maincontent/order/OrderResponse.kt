7234a1aed72be1bfbc413e32de675259ea1831b1
Ikrima Amanda <ikrima.amanda.07@gmail.com>
2021-01-29 09:38:00 +0800

    integrasi api order

--- app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/order/OrderResponse.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/order/OrderResponse.kt
@@ -2,17 +2,18 @@ package com.istekno.coffeebreakapp.main.maincontent.order
 
 import com.google.gson.annotations.SerializedName
 
-data class OrderResponse(val success: Boolean, val message: String, val data: List<DataProduct>) {
-    data class DataProduct(
-            @SerializedName("pr_id") val productId: Int,
-            @SerializedName("dc_id") val discountId: Int,
-            @SerializedName("pr_name") val productName: String,
-            @SerializedName("pr_desc") val productDesc: String,
-            @SerializedName("pr_unit_price") val productPrice: String,
-            @SerializedName("pr_image") val productImage: String,
-            @SerializedName("pr_favorite") val productFavorite: String,
-            @SerializedName("pr_category") val productCategory: String,
-            @SerializedName("pr_created_at") val productCreated: String,
-            @SerializedName("pr_updated_at") val productUpdated: String
-    )
+data class OrderResponse(val success: Boolean, val message: String, val data: List<Data>) {
+        data class Data(
+                @SerializedName("od_id") val orderDetailId: Int,
+                @SerializedName("cs_id") val customerId: Int,
+                @SerializedName("dv_id") val deliveryId: Int,
+                @SerializedName("od_total_price_before_tax") val priceBeforeTax: Int,
+                @SerializedName("co_id") val couponId: Int,
+                @SerializedName("od_totalPrice") val totalPrice: Int,
+                @SerializedName("od_status") val orderDetailStatus: String,
+                @SerializedName("od_payment_method") val orderPayment: String,
+                @SerializedName("od_tax") val orderTax: Int,
+                @SerializedName("od_created_at") val orderCreated: String,
+                @SerializedName("od_updated_at") val orderUpdated: String
+        )
 }
--- app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/order/OrderViewModel.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/order/OrderViewModel.kt
@@ -1,13 +1,66 @@
 package com.istekno.coffeebreakapp.main.maincontent.order
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import com.istekno.coffeebreakapp.utilities.SharedPreferenceUtil
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 class OrderViewModel: ViewModel(), CoroutineScope {
 
+    val listData = MutableLiveData<List<OrderResponse.Data>>()
+    val getListData = MutableLiveData<Boolean>()
+    val isLoading = MutableLiveData<Boolean>()
+    private val isNotFound = MutableLiveData<Boolean>()
+
     override val coroutineContext: CoroutineContext
         get() = Job() + Dispatchers.Main
+
+    private lateinit var service: OrderService
+    private lateinit var sharedPref : SharedPreferenceUtil
+
+    fun setService(service: OrderService) {
+        this.service = service
+    }
+
+    fun setSharedPref(sharedPref: SharedPreferenceUtil) {
+        this.sharedPref = sharedPref
+    }
+
+    fun callOrderApi() {
+        launch {
+            isLoading.value = true
+
+            val result = withContext(Dispatchers.IO) {
+                try {
+                    service.getOrderByCsId(sharedPref.getPreference().roleID!!)
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+
+                    withContext(Dispatchers.Main) {
+                        getListData.value = false
+                        isLoading.value = false
+                        isNotFound.value = true
+                    }
+                }
+            }
+
+            if (result is OrderResponse) {
+                if (result.success) {
+                    getListData.value = result.success
+                    val list = result.data.map {
+                        OrderResponse.Data(it.orderDetailId, it.customerId, it.deliveryId, it.priceBeforeTax, it.couponId, it.totalPrice, it.orderDetailStatus, it.orderPayment, it.orderTax, it.orderCreated, it.orderUpdated)
+                    }
+                    val mutable = list.toMutableList()
+                    mutable.removeAll { it.orderDetailStatus == "Done" }
+                    listData.value = mutable
+                    isLoading.value = false
+                } else {
+                    getListData.value = false
+                    isLoading.value = false
+                }
+            }
+        }
+    }
+
 }
