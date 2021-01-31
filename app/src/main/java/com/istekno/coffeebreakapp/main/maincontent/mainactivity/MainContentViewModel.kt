83c41e59f018675165abbed7a5298d9e3d9b4603
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-01-31 14:52:08 +0700

    add search

--- app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/mainactivity/MainContentViewModel.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/main/maincontent/mainactivity/MainContentViewModel.kt
@@ -1,12 +1,18 @@
 package com.istekno.coffeebreakapp.main.maincontent.mainactivity
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.istekno.coffeebreakapp.main.maincontent.homepage.HomeResponse
 import com.istekno.coffeebreakapp.model.SharedPrefModel
 import com.istekno.coffeebreakapp.utilities.SharedPreferenceUtil
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 class MainContentViewModel: ViewModel(), CoroutineScope {
+
+    var listProduct = MutableLiveData<List<HomeResponse.DataProduct>>()
+    val productAction = MutableLiveData<Boolean>()
+    val isFailedStatus = MutableLiveData<Boolean>()
 
     override val coroutineContext: CoroutineContext
         get() = Job() + Dispatchers.Main
@@ -44,7 +50,50 @@ class MainContentViewModel: ViewModel(), CoroutineScope {
         }
     }
 
+    fun getAllProductByQuery(type: Int, query: String, filter: Int) {
+        launch {
 
+            productAction.value = true
+            isFailedStatus.value = false
 
+            val result = withContext(Job() + Dispatchers.IO) {
+                try {
 
+                    if (type == 0) {
+                        service.getProductByQuery(" ")
+                    } else {
+                        service.getProductByQuery(query)
+                    }
+
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+
+                    withContext(Job() + Dispatchers.Main) {
+                        productAction.value = false
+                        isFailedStatus.value = true
+                    }
+                }
+            }
+
+            if (result is HomeResponse) {
+                var mutableList = mutableListOf<HomeResponse.DataProduct>()
+                val list = result.data.map {
+                    HomeResponse.DataProduct(it.productId, it.discountId, it.productName, it.productDesc, it.productPrice, it.productImage, it.productFavorite, it.productCategory, it.productCreated, it.productUpdated)
+                }
+                mutableList = list.toMutableList()
+
+                when(filter) {
+                    1 -> { mutableList.removeIf { it.productCategory != "Food" } }
+                    2 -> { mutableList.removeIf { it.productCategory != "Drink" } }
+                }
+
+                if (mutableList.isNullOrEmpty()) {
+                    isFailedStatus.value = true
+                }
+
+                listProduct.value = mutableList
+                productAction.value = false
+            }
+        }
+    }
 }
