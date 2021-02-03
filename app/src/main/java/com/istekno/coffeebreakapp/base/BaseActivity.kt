542699ff39b62c12137b5d93058c2ea3e24f9489
ajizul22 <70998823+ajizul22@users.noreply.github.com>
2021-02-04 01:18:31 +0700

    check connection

--- app/src/main/java/com/istekno/coffeebreakapp/base/BaseActivity.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/base/BaseActivity.kt
@@ -2,19 +2,34 @@ package com.istekno.coffeebreakapp.base
 
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.istekno.coffeebreakapp.utilities.Dialog
 
 abstract class BaseActivity<ActivityBinding : ViewDataBinding> : AppCompatActivity() {
     protected lateinit var binding: ActivityBinding
     protected var setLayout = 0
+    private lateinit var dialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, setLayout)
+        dialog = Dialog()
+
+    connectionCheck(this)
+}
+
+private fun connectionCheck(context: Context) {
+    val connectManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val checkNetwork = connectManager.activeNetworkInfo
+
+    if (checkNetwork == null || !checkNetwork.isConnected || !checkNetwork.isAvailable) {
+        dialog.dialogCheckInternet(this, this)
     }
+}
 
     protected inline fun <reified ActivityClass> intent(context: Context) {
         context.startActivity(Intent(context, ActivityClass::class.java))
--- app/src/main/java/com/istekno/coffeebreakapp/base/BaseActivityViewModel.kt
+++ app/src/main/java/com/istekno/coffeebreakapp/base/BaseActivityViewModel.kt
@@ -2,11 +2,13 @@ package com.istekno.coffeebreakapp.base
 
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
+import com.istekno.coffeebreakapp.utilities.Dialog
 
 abstract class BaseActivityViewModel<ActivityBinding : ViewDataBinding, ActivityViewModel : ViewModel> :
     AppCompatActivity() {
@@ -15,11 +17,25 @@ abstract class BaseActivityViewModel<ActivityBinding : ViewDataBinding, Activity
     lateinit var viewModel: ActivityViewModel
     protected var setLayout = 0
     protected var setViewModel: ActivityViewModel? = null
+    private lateinit var dialog: Dialog
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, setLayout)
         viewModel = setViewModel!!
+        dialog = Dialog()
+
+        connectionCheck(this)
+    }
+
+    private fun connectionCheck(context: Context) {
+        val connectManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val checkNetwork = connectManager.activeNetworkInfo
+
+        if (checkNetwork == null || !checkNetwork.isConnected || !checkNetwork.isAvailable) {
+            dialog.dialogCheckInternet(this, this)
+        }
     }
 
     protected inline fun <reified ActivityClass> intent(context: Context) {
